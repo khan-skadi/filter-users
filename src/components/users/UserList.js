029b9936981bd5894bc85f17e80f27307c5a7290
khan-skadi <kartalov.pt@gmail.com>
2020-11-29 22:04:45 +0100

    fix the last name included in filter

--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -5,7 +5,7 @@ const UserList = ({ users, inputValue, showCountry }) => {
   const renderList = (userList) => {
     if (inputValue.length >= 3) {
       const filteredUsers = userList.filter((user) =>
-        user.firstname.concat(user.lastname).toLowerCase().includes(inputValue)
+        user.firstname.concat(` ${user.lastname}`).toLowerCase().includes(inputValue)
       );
       const count = filteredUsers.length;
       return (
